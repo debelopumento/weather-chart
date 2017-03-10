801c9ccea82f2c7ce1e3ccf4e42923d2efa601d3
debelopumento <imdiye@gmail.com>
2017-03-09 21:42:25 -0800

    added d3 canvas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import d3 from 'd3'
+
+
+class Svg extends Component {
+	render() {
+		return (    
+			<div>
+				<svg id="visualisation" width="1000" height="500">
+					<rect width="100%" height="100%" fill="pink"></rect>
+				</svg>
+			</div>
+		)
+	}
+}
+
 
 ReactDOM.render(
-  <App />,
+	//<App />,
+	<Svg />,
   document.getElementById('root')
 );
