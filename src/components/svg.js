da6b58b53eda84e0ef8f938effcaeb66df66b8e7
debelopumento <imdiye@gmail.com>
2017-03-13 23:21:43 -0700

    drew line and lines up with 24-hour stand-in xAxis

--- src/components/svg.js
+++ src/components/svg.js
@@ -1,42 +1,46 @@
-import React, {Component} from 'react'
+import React, { PureComponent, PropTypes } from 'react'
 import * as d3 from 'd3'
+import { connect } from 'react-redux'
 import axios from 'axios'
 import {getCurrentWeather} from '../actions/actionIndex'
 import store from '../store'
+import * as actions from '../actions/actionIndex'
+
+const { object, func } = PropTypes
 
 
-
-class Svg extends Component {
+class Svg extends PureComponent {
   
-  constructor(props) {
-      super(props)
-      this.state = {
-          location: "San_Francisco",
-          currentWeather: null,
-          historyWeather: null
-      }
-
-      store.subscribe(() => {
-          this.setState({
-              currentWeather: store.getState().currentWeather,
-              historyWeather: store.getState().historyWeather
-          })
-      })
-  }
 
   fetchData(event) {
     console.log(6, this.state)
   }
 
 
+  state = {
+      location: "San_Francisco",
+      currentWeather: null,
+      historyWeather: null
+  }
+
+
 
   componentWillMount() {
+    
+    store.subscribe(() => {
+        this.setState({
+            currentWeather: store.getState().currentWeather,
+            historyWeather: store.getState().historyWeather
+        })
+    })
     console.log(8)
     store.dispatch(getCurrentWeather())
     console.log(10)
   }
 
+
   componentDidMount() {
+    const WIDTH = document.documentElement.clientWidth  
     const HEIGHT = 400
     const MARGINS = {
       top: 20,
@@ -60,26 +64,38 @@ class Svg extends Component {
         */
         const currentWeatherData = [
             {"temperature": Number(data.hourly_forecast[0].temp.english),
-             "time": (data.hourly_forecast[0].FCTTIME.hour)
-            },
-            {"temperature": Number(data.hourly_forecast[1].temp.english),
-             "time": (data.hourly_forecast[1].FCTTIME.hour)
-            },
-            {"temperature": Number(data.hourly_forecast[2].temp.english),
-             "time": (data.hourly_forecast[2].FCTTIME.hour)
+             "time": 0
             },
             {"temperature": Number(data.hourly_forecast[3].temp.english),
-             "time": (data.hourly_forecast[3].FCTTIME.hour)
+             "time": 3
             },
-            {"temperature": Number(data.hourly_forecast[4].temp.english),
-             "time": (data.hourly_forecast[4].FCTTIME.hour)
+            {"temperature": Number(data.hourly_forecast[6].temp.english),
+             "time": 6
+            },
+            {"temperature": Number(data.hourly_forecast[9].temp.english),
+             "time": 9
+            },
+            {"temperature": Number(data.hourly_forecast[12].temp.english),
+             "time": 12
+            },
+            {"temperature": Number(data.hourly_forecast[15].temp.english),
+             "time": 15
+            },
+            {"temperature": Number(data.hourly_forecast[18].temp.english),
+             "time": 18
+            },
+            {"temperature": Number(data.hourly_forecast[21].temp.english),
+             "time": 21
+            },
+            {"temperature": Number(data.hourly_forecast[24].temp.english),
+             "time": 24
             }
         ]
         console.log(21, currentWeatherData)
-        const xScale = d3.scaleLinear().range([20, 480]).domain([currentWeatherData[0].time, currentWeatherData[4].time])
-        const yScale = d3.scaleLinear().range([480, 20]).domain([40, 70])
-        const xAxis = d3.axisBottom().scale(xScale).ticks(6)
-        const yAxis = d3.axisLeft().scale(yScale)
+        const xScale = d3.scaleLinear().range([20, 600]).domain([currentWeatherData[0].time, currentWeatherData[8].time])
+        
+        const standinXaxis = d3.axisBottom().scale(xScale).ticks(7)
+        
         const vis = d3.select("#visualisation")
         vis.append("rect")
             .attr("width", "100%")
@@ -89,13 +105,11 @@ class Svg extends Component {
         // stand-in x axis
         vis.append("svg:g")
             .attr("class", "x axis")
-            .attr("transform", "translate(30," + (HEIGHT - MARGINS.bottom) + ")")
-            .call(xAxis);
+            .attr("transform", "translate(50, 300)")
+            .call(standinXaxis);
         //
 
         //draw x axis
-        const timezoneOffset = (new Date()).getTimezoneOffset() * 60000
-        console.log(6, timezoneOffset)
         const now = new Date()
         let today = now
         today.setDate(now.getDate() - 0.29166666)
@@ -104,16 +118,15 @@ class Svg extends Component {
         tomorrow.setDate(now.getDate() + 1)
         tomorrow = tomorrow.toISOString().slice(0, 19)
         console.log(4, today, 5, tomorrow)
-        let i = 0;
-        let thisScale;
         const d3ParseScale = d3.scaleTime()
             .domain([d3.timeParse('%Y-%m-%dT%H:%M:%S')(today), d3.timeParse('%Y-%m-%dT%H:%M:%S')(tomorrow)])
-            .range([40, 480]);
+            .range([50, 600]);
 
         const drawScale = function(scale, dst) {
             const xAxis = d3.axisBottom(scale);
             const scaleGroup = vis.append('svg:g')
                 .attr('class', 'x axis')
+                .attr("transform", "translate(0, 380)")
                 .call(xAxis);
             return scaleGroup;
         }
@@ -122,27 +135,37 @@ class Svg extends Component {
         //
 
         //draw y axis
+        const yScale = d3.scaleLinear().range([380, 20]).domain([40, 80])
+        const yAxis = d3.axisLeft().scale(yScale)
         vis.append("svg:g")
-                .attr("class", "y axis")
-                .attr("transform", "translate(" + (MARGINS.left) + ",0)")
-                .call(yAxis);
-                
+              .attr("class", "y axis")
+              .attr("transform", "translate(50, 0)")
+              .call(yAxis);
+        
+        //draw path        
         const lineGen = d3.line()
-                .x(function(d) {
-                    return xScale(d.time);
-                })
-                .y(function(d) {
-                    return yScale(d.temperature);
-                })
-                .curve(d3.curveBasis);
+              .x(function(d) {
+                  return xScale(d.time);
+              })
+              .y(function(d) {
+                  return yScale(d.temperature);
+              })
+              .curve(d3.curveCardinal);
 
         vis.append('svg:path')
               .attr('d', lineGen(currentWeatherData))
-              .attr('stroke', 'green')
-              .attr('stroke-width', 2)
-              .attr('fill', 'none'); 
+              .attr('stroke', 'black')
+              .attr("transform", "translate(50, 0)")
+              .attr('stroke-width', 1)
+              .attr('fill', 'none');
 
-        
+        vis.append('svg:dot')
+              .data(currentWeatherData)
+              .enter().append('circle')
+              .attr('r', 3.5)
+              .attr('cx', function(d) { return xScale(d.time)})
+              .attr('cy', function(d) { return yScale(d.temperature)})
+
       })
 
   }
@@ -161,8 +184,10 @@ class Svg extends Component {
 
     return (    
       <div>
-        <svg id='visualisation' width={WIDTH} height={HEIGHT}>
+        <svg id='visualisation' width={WIDTH-MARGINS.right-MARGINS.left} height={HEIGHT}>
+
         </svg>
+
         <input type='submit' value="clickMe" onClick={this.fetchData} />
       </div>
     )
